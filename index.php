e138e98d21eb5283a182b9eac3246c652d727f1d
AbhIndra <abhimanyu4u@outlook.com>
2017-04-20 15:10:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
 
-  echo "<h1> Hi $_GET['nm']!</h1>";
+  echo "<h1> Hi $_GET['nm']</h1>";
   
  ?>
-<form method="GET">
+<form method="GET" action="index.jsp">
 Please enter your name : <input type = "text" name="nm">
+  <input type="submit" value= "Submit">
 </form>
 
