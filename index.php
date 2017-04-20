b129756ac0384c9ba55411d2c3201e3817d3e043
AbhIndra <abhimanyu4u@outlook.com>
2017-04-20 15:04:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
 
-if(isset($_GET['nm']))
- {
+
   echo "<h1> Hi $_GET['nm']!</h1>";
   
- }
- else
- {
-   echo "Hello World!";
- }
  ?>
 <form method="GET">
 Please enter your name : <input type = "text" name="nm">
